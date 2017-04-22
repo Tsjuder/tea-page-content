7ff7d015c61c07a36c5bc523378525b14a8f0ff4
Tsjuder <raymondcostner@gmail.com>
2017-04-22 04:53:00 +0400

    Release version 1.3.0

--- config.php
+++ config.php
@@ -1,96 +1,169 @@
 <?php
-/**
- * @package Tea Page Content
- * @version 1.2.3
- */
 
 return array(
-	// Predefined default values, e.g. default parameters
-	'defaults' => array(
-		'widget' => array(
-			'title' => '',
-			'posts' => '',
-			'show_page_thumbnail' => true,
-			'show_page_content' => true,
-			'show_page_title' => true,
-			'linked_page_title' => false,
-			'linked_page_thumbnail' => false,
-			'template' => 'default',
-			'order' => 'desc',
-			'per-page' => -1,
+    // Predefined default values, e.g. default parameters
+    'defaults' => array(
+        'widget' => array(
+            'title' => '',
+            'posts' => '',
+            'show_page_thumbnail' => true,
+            'show_page_content' => true,
+            'show_page_title' => true,
+            'linked_page_title' => false,
+            'linked_page_thumbnail' => false,
+            'template' => 'default',
 
-			'caller' => 'widget',
-		),
-		'shortcode' => array(
-			'title' => '',
-			'posts' => '',
-			'id' => '', // @deprecated since 1.1
-			'template' => 'default',
-			'show_page_thumbnail' => true,
-			'show_page_content' => true,
-			'show_page_title' => true,
-			'linked_page_title' => false,
-			'linked_page_thumbnail' => false,
-			'order' => 'desc',
+            'order' => 'desc',
+            'orderby' => 'date',
 
-			'caller' => 'shortcode',
-		),
-		'post-types' => array(
-			'public' => true,
-		),
-		'posts' => array(
-			'post_status' => 'publish', // may be an array too
-			'has_password' => false,
-		),
-		'template-variables' => array(
-			'type' => 'text',
-			'defaults' => '',
-		),
-		'page-variables' => array(
-			'page_title' => array( // @todo include {prefix} placeholder for DRY
-				'type' => 'text',
-				'title' => __('Title', 'tea-page-content')
-			),
-			'page_content' => array(
-				'type' => 'textarea',
-				'title' => __('Content', 'tea-page-content')
-			),
-			'page_thumbnail' => array(
-				'type' => 'media',
-				'title' => __('Thumbnail', 'tea-page-content')
-			)
-		)
-	),
+            'caller' => 'widget',
+        ),
 
-	// Predefined system values, e.g. logical operators, needly constants or system paths
-	'system' => array(
-		'posts' => array(
-			'types-operator' => 'or',
-		),
-		'predefined-templates' => array(
-			// @deprecated default-padded template, since 1.1
-			'default', 'default-padded', 'bootstrap-3', 'bootstrap-4', 'waterfall'
-		),
-		'template-variables' => array(
-			'mask' => array(
-				'name', 'type', 'defaults'
-			),
-		),
-		'page-variables' => array(
-			'prefix' => 'page_'
-		),
-		'template-directories' => array(
-			'plugin' => TEA_PAGE_CONTENT_PATH . '/templates/',
-			'theme' => get_stylesheet_directory() . '/templates/'
-		),
-		'versions' => array(
-			'plugin' => '1.2.3',
-			'scripts' => '1.2.3',
-			'styles' => '1.2.3'
-		),
+        'shortcode' => array(
+            'title' => '',
+            'posts' => '',
+            'template' => 'default',
+            'show_page_thumbnail' => true,
+            'show_page_content' => true,
+            'show_page_title' => true,
+            'linked_page_title' => false,
+            'linked_page_thumbnail' => false,
 
-		'settings' => array(
-			'include-css' => true,
-		),
-	)
+            'order' => 'desc',
+            'orderby' => 'date',
+
+            'caller' => 'shortcode',
+        ),
+
+        'post-types' => array(
+            'public' => true,
+        ),
+
+        'posts' => array(
+            'post_status' => 'publish', // may be an array too
+            'has_password' => false,
+            'posts_per_page' => -1,
+            'order' => 'desc',
+            'orderby' => 'date',
+            'post__in' => [],
+        ),
+
+        'template-variables' => array(
+            'type' => 'text',
+            'defaults' => '',
+        ),
+        'page-variables' => array(
+            'page_title' => array(
+                'type' => 'text',
+                'title' => __('Title', 'tea-page-content')
+            ),
+            'page_content' => array(
+                'type' => 'textarea',
+                'title' => __('Content', 'tea-page-content')
+            ),
+            'page_thumbnail' => array(
+                'type' => 'media',
+                'title' => __('Thumbnail', 'tea-page-content')
+            ),
+        ),
+
+        'thumbnails' => [
+            'size' => 'post-thumbnail',
+        ],
+
+        'templates' => [
+            'admin' => [
+                'partials' => [
+                    'template-variables' => [
+                        'name' => 'template-variables',
+                        'path' => '{admin}/partials/',
+                    ],
+                ],
+                'modals' => [
+                    'page-variables' => [
+                        'name' => 'page-variables',
+                        'path' => '{admin}/modals/',
+                    ],
+                    'insert-shortcode' => [
+                        'name' => 'insert-shortcode',
+                        'path' => '{admin}/modals/',
+                    ],
+                ],
+                'pages' => [
+                    'settings' => [
+                        'name' => 'settings',
+                        'path' => '{admin}/pages/',
+                    ]
+                ],
+                'modules' => [
+                    'widget' => [
+                        'name' => 'widget',
+                        'path' => '{admin}/modules/',
+                    ]
+                ],
+            ],
+
+            'client' => [
+                'default-template' => 'default',
+                'widget' => [
+                    'name' => 'widget',
+                    'path' => '{client}/modules/',
+                ],
+            ],
+        ],
+    ),
+
+    // Predefined system values, e.g. logical operators, needly constants or system paths
+    'system' => array(
+        'posts' => array(
+            'types-operator' => 'or',
+        ),
+        'predefined-templates' => array(
+            'default', 'bootstrap-3', 'bootstrap-4', 'waterfall'
+        ),
+        'template-variables' => array(
+            'mask' => [
+                'structure' => [
+                    'name', 'type', 'defaults'
+                ],
+                'placeholder' => '{mask}'
+            ]
+        ),
+        'page-variables' => array(
+            'prefix' => 'page_'
+        ),
+        'template-directories' => array(
+            'plugin' => [
+                'placeholder' => '{plugin}',
+                'path' => \TeaPageContent\PLUGIN_PATH,
+            ],
+
+            'admin' => [
+                'placeholder' => '{admin}',
+                'path' => \TeaPageContent\PLUGIN_PATH . '/templates/admin',
+            ],
+            'client' => [
+                'placeholder' => '{client}',
+                'path' => \TeaPageContent\PLUGIN_PATH . '/templates/client',
+            ],
+            'layouts' => [
+                'placeholder' => '{layouts}',
+                'path' => \TeaPageContent\PLUGIN_PATH . '/templates/client/layouts',
+            ],
+
+            'theme' => [
+                'placeholder' => '{theme}',
+                'path' => get_stylesheet_directory() . '/templates',
+            ],
+        ),
+        'versions' => array(
+            'plugin' => '1.3.0',
+            'scripts' => '1.2.3',
+            'styles' => '1.2.3',
+        ),
+
+        'settings' => array(
+            'include-css' => true,
+        ),
+    )
 );
